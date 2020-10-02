d949c6e19df1adf30d12d495d65e1848794e1414
KushanTharaka97 <tharakakushan@gmail.com>
2020-10-02 11:07:34 +0530

    Custom Exception CREATED

--- src/main/java/com/example/qanda/demo/qanda/repository/QandARepository.java
+++ src/main/java/com/example/qanda/demo/qanda/repository/QandARepository.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Repository;
 
 import com.example.qanda.demo.qanda.model.QandAEntity;
 
+//The Spring JPA comes up with a JpaRepository interface .
+// This interface defines all the methods used for CRUD operations on the entity.
 @Repository
 public interface QandARepository extends JpaRepository<QandAEntity,Long>{
 
